9942ddec978dcb842bf39e8a842434700723f11e
jerome-cossu <jerome.cossu@laplateforme.io>
2025-09-09 15:01:00 +0200

    🗃️ feat : add dadta base

---
+++ overlook_hotel/data/hotel_db.sql
@@ -0,0 +1,762 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 17.4
+-- Dumped by pg_dump version 17.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: rating_enum; Type: TYPE; Schema: public; Owner: postgres
+--
+
+CREATE TYPE public.rating_enum AS ENUM (
+    '1',
+    '2',
+    '3',
+    '4',
+    '5'
+);
+
+
+ALTER TYPE public.rating_enum OWNER TO postgres;
+
+--
+-- Name: role; Type: TYPE; Schema: public; Owner: postgres
+--
+
+CREATE TYPE public.role AS ENUM (
+    'custumer',
+    'employee',
+    'manager'
+);
+
+
+ALTER TYPE public.role OWNER TO postgres;
+
+--
+-- Name: shift_type; Type: TYPE; Schema: public; Owner: postgres
+--
+
+CREATE TYPE public.shift_type AS ENUM (
+    'working',
+    'vacation',
+    'sick_leave',
+    'training'
+);
+
+
+ALTER TYPE public.shift_type OWNER TO postgres;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: bookings; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.bookings (
+    booking_id integer NOT NULL,
+    user_id integer NOT NULL,
+    room_id integer NOT NULL,
+    check_in date NOT NULL,
+    check_out date NOT NULL,
+    CONSTRAINT date_valid CHECK ((check_out > check_in))
+);
+
+
+ALTER TABLE public.bookings OWNER TO postgres;
+
+--
+-- Name: bookings_booking_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.bookings_booking_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.bookings_booking_id_seq OWNER TO postgres;
+
+--
+-- Name: bookings_booking_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.bookings_booking_id_seq OWNED BY public.bookings.booking_id;
+
+
+--
+-- Name: events; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.events (
+    event_id integer NOT NULL,
+    name character varying(50) NOT NULL,
+    description character varying(500)
+);
+
+
+ALTER TABLE public.events OWNER TO postgres;
+
+--
+-- Name: events_booking; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.events_booking (
+    event_booking_id integer NOT NULL,
+    client_id integer NOT NULL,
+    event_id integer NOT NULL,
+    facility_id integer NOT NULL,
+    start_date date NOT NULL,
+    end_date date NOT NULL,
+    CONSTRAINT valid_event_dates CHECK ((end_date > start_date))
+);
+
+
+ALTER TABLE public.events_booking OWNER TO postgres;
+
+--
+-- Name: events_booking_event_booking_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.events_booking_event_booking_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.events_booking_event_booking_id_seq OWNER TO postgres;
+
+--
+-- Name: events_booking_event_booking_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.events_booking_event_booking_id_seq OWNED BY public.events_booking.event_booking_id;
+
+
+--
+-- Name: events_event_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.events_event_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.events_event_id_seq OWNER TO postgres;
+
+--
+-- Name: events_event_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.events_event_id_seq OWNED BY public.events.event_id;
+
+
+--
+-- Name: facilities; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.facilities (
+    facility_id integer NOT NULL,
+    name character varying(50) NOT NULL,
+    description character varying(500)
+);
+
+
+ALTER TABLE public.facilities OWNER TO postgres;
+
+--
+-- Name: facilities_facility_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.facilities_facility_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.facilities_facility_id_seq OWNER TO postgres;
+
+--
+-- Name: facilities_facility_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.facilities_facility_id_seq OWNED BY public.facilities.facility_id;
+
+
+--
+-- Name: reviews; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.reviews (
+    review_id integer NOT NULL,
+    user_id integer NOT NULL,
+    room_id integer NOT NULL,
+    rating public.rating_enum DEFAULT '1'::public.rating_enum NOT NULL,
+    comment character varying(500)
+);
+
+
+ALTER TABLE public.reviews OWNER TO postgres;
+
+--
+-- Name: reviews_review_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.reviews_review_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.reviews_review_id_seq OWNER TO postgres;
+
+--
+-- Name: reviews_review_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.reviews_review_id_seq OWNED BY public.reviews.review_id;
+
+
+--
+-- Name: rooms; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.rooms (
+    room_id integer NOT NULL,
+    room_number integer NOT NULL,
+    guest integer NOT NULL,
+    occupency boolean NOT NULL,
+    features character varying(100) NOT NULL,
+    price real NOT NULL
+);
+
+
+ALTER TABLE public.rooms OWNER TO postgres;
+
+--
+-- Name: rooms_room_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.rooms_room_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.rooms_room_id_seq OWNER TO postgres;
+
+--
+-- Name: rooms_room_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.rooms_room_id_seq OWNED BY public.rooms.room_id;
+
+
+--
+-- Name: schedules; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.schedules (
+    schedule_id integer NOT NULL,
+    user_id integer NOT NULL,
+    shift_id integer NOT NULL,
+    schedule_date date NOT NULL
+);
+
+
+ALTER TABLE public.schedules OWNER TO postgres;
+
+--
+-- Name: schedules_schedule_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.schedules_schedule_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.schedules_schedule_id_seq OWNER TO postgres;
+
+--
+-- Name: schedules_schedule_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.schedules_schedule_id_seq OWNED BY public.schedules.schedule_id;
+
+
+--
+-- Name: shifts; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.shifts (
+    shift_id integer NOT NULL,
+    employee_name character varying(100) NOT NULL,
+    start_time timestamp without time zone NOT NULL,
+    end_time timestamp without time zone NOT NULL,
+    type public.shift_type DEFAULT 'working'::public.shift_type NOT NULL,
+    CONSTRAINT valid_shift CHECK ((end_time > start_time))
+);
+
+
+ALTER TABLE public.shifts OWNER TO postgres;
+
+--
+-- Name: shifts_shift_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.shifts_shift_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.shifts_shift_id_seq OWNER TO postgres;
+
+--
+-- Name: shifts_shift_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.shifts_shift_id_seq OWNED BY public.shifts.shift_id;
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    user_id integer NOT NULL,
+    role public.role DEFAULT 'custumer'::public.role NOT NULL,
+    first_name character varying(80) NOT NULL,
+    last_name character varying(80) NOT NULL,
+    email character varying(80) NOT NULL,
+    password character varying(80) NOT NULL,
+    loyalty_points integer
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- Name: users_user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.users_user_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.users_user_id_seq OWNER TO postgres;
+
+--
+-- Name: users_user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.users_user_id_seq OWNED BY public.users.user_id;
+
+
+--
+-- Name: bookings booking_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.bookings ALTER COLUMN booking_id SET DEFAULT nextval('public.bookings_booking_id_seq'::regclass);
+
+
+--
+-- Name: events event_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.events ALTER COLUMN event_id SET DEFAULT nextval('public.events_event_id_seq'::regclass);
+
+
+--
+-- Name: events_booking event_booking_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.events_booking ALTER COLUMN event_booking_id SET DEFAULT nextval('public.events_booking_event_booking_id_seq'::regclass);
+
+
+--
+-- Name: facilities facility_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.facilities ALTER COLUMN facility_id SET DEFAULT nextval('public.facilities_facility_id_seq'::regclass);
+
+
+--
+-- Name: reviews review_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.reviews ALTER COLUMN review_id SET DEFAULT nextval('public.reviews_review_id_seq'::regclass);
+
+
+--
+-- Name: rooms room_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.rooms ALTER COLUMN room_id SET DEFAULT nextval('public.rooms_room_id_seq'::regclass);
+
+
+--
+-- Name: schedules schedule_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.schedules ALTER COLUMN schedule_id SET DEFAULT nextval('public.schedules_schedule_id_seq'::regclass);
+
+
+--
+-- Name: shifts shift_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.shifts ALTER COLUMN shift_id SET DEFAULT nextval('public.shifts_shift_id_seq'::regclass);
+
+
+--
+-- Name: users user_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.users_user_id_seq'::regclass);
+
+
+--
+-- Data for Name: bookings; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.bookings (booking_id, user_id, room_id, check_in, check_out) FROM stdin;
+\.
+
+
+--
+-- Data for Name: events; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.events (event_id, name, description) FROM stdin;
+\.
+
+
+--
+-- Data for Name: events_booking; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.events_booking (event_booking_id, client_id, event_id, facility_id, start_date, end_date) FROM stdin;
+\.
+
+
+--
+-- Data for Name: facilities; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.facilities (facility_id, name, description) FROM stdin;
+\.
+
+
+--
+-- Data for Name: reviews; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.reviews (review_id, user_id, room_id, rating, comment) FROM stdin;
+\.
+
+
+--
+-- Data for Name: rooms; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.rooms (room_id, room_number, guest, occupency, features, price) FROM stdin;
+\.
+
+
+--
+-- Data for Name: schedules; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.schedules (schedule_id, user_id, shift_id, schedule_date) FROM stdin;
+\.
+
+
+--
+-- Data for Name: shifts; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.shifts (shift_id, employee_name, start_time, end_time, type) FROM stdin;
+\.
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (user_id, role, first_name, last_name, email, password, loyalty_points) FROM stdin;
+\.
+
+
+--
+-- Name: bookings_booking_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.bookings_booking_id_seq', 1, false);
+
+
+--
+-- Name: events_booking_event_booking_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.events_booking_event_booking_id_seq', 1, false);
+
+
+--
+-- Name: events_event_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.events_event_id_seq', 1, false);
+
+
+--
+-- Name: facilities_facility_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.facilities_facility_id_seq', 1, false);
+
+
+--
+-- Name: reviews_review_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.reviews_review_id_seq', 1, false);
+
+
+--
+-- Name: rooms_room_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.rooms_room_id_seq', 1, false);
+
+
+--
+-- Name: schedules_schedule_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.schedules_schedule_id_seq', 1, false);
+
+
+--
+-- Name: shifts_shift_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.shifts_shift_id_seq', 1, false);
+
+
+--
+-- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.users_user_id_seq', 1, false);
+
+
+--
+-- Name: bookings bookings_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.bookings
+    ADD CONSTRAINT bookings_pkey PRIMARY KEY (booking_id);
+
+
+--
+-- Name: events_booking events_booking_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.events_booking
+    ADD CONSTRAINT events_booking_pkey PRIMARY KEY (event_booking_id);
+
+
+--
+-- Name: events events_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.events
+    ADD CONSTRAINT events_pkey PRIMARY KEY (event_id);
+
+
+--
+-- Name: facilities facilities_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.facilities
+    ADD CONSTRAINT facilities_pkey PRIMARY KEY (facility_id);
+
+
+--
+-- Name: reviews reviews_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.reviews
+    ADD CONSTRAINT reviews_pkey PRIMARY KEY (review_id);
+
+
+--
+-- Name: rooms rooms_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.rooms
+    ADD CONSTRAINT rooms_pkey PRIMARY KEY (room_id);
+
+
+--
+-- Name: rooms rooms_room_number_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.rooms
+    ADD CONSTRAINT rooms_room_number_key UNIQUE (room_number);
+
+
+--
+-- Name: schedules schedules_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.schedules
+    ADD CONSTRAINT schedules_pkey PRIMARY KEY (schedule_id);
+
+
+--
+-- Name: shifts shifts_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.shifts
+    ADD CONSTRAINT shifts_pkey PRIMARY KEY (shift_id);
+
+
+--
+-- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_email_key UNIQUE (email);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (user_id);
+
+
+--
+-- Name: events_booking fk_client; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.events_booking
+    ADD CONSTRAINT fk_client FOREIGN KEY (client_id) REFERENCES public.users(user_id) ON DELETE CASCADE;
+
+
+--
+-- Name: events_booking fk_event; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.events_booking
+    ADD CONSTRAINT fk_event FOREIGN KEY (event_id) REFERENCES public.events(event_id) ON DELETE CASCADE;
+
+
+--
+-- Name: events_booking fk_facility; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.events_booking
+    ADD CONSTRAINT fk_facility FOREIGN KEY (facility_id) REFERENCES public.facilities(facility_id) ON DELETE CASCADE;
+
+
+--
+-- Name: bookings fk_room; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.bookings
+    ADD CONSTRAINT fk_room FOREIGN KEY (room_id) REFERENCES public.rooms(room_id) ON DELETE CASCADE;
+
+
+--
+-- Name: reviews fk_room_review; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.reviews
+    ADD CONSTRAINT fk_room_review FOREIGN KEY (room_id) REFERENCES public.rooms(room_id) ON DELETE CASCADE;
+
+
+--
+-- Name: schedules fk_schedule_shift; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.schedules
+    ADD CONSTRAINT fk_schedule_shift FOREIGN KEY (shift_id) REFERENCES public.shifts(shift_id) ON DELETE CASCADE;
+
+
+--
+-- Name: schedules fk_schedule_user; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.schedules
+    ADD CONSTRAINT fk_schedule_user FOREIGN KEY (user_id) REFERENCES public.users(user_id) ON DELETE CASCADE;
+
+
+--
+-- Name: bookings fk_user; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.bookings
+    ADD CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES public.users(user_id) ON DELETE CASCADE;
+
+
+--
+-- Name: reviews fk_user_review; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.reviews
+    ADD CONSTRAINT fk_user_review FOREIGN KEY (user_id) REFERENCES public.users(user_id) ON DELETE CASCADE;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
