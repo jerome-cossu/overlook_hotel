58dcf3f0b508c20c071ce443055e0165af248d7d
jerome-cossu <jerome.cossu@laplateforme.io>
2025-09-10 13:06:38 +0200

    🗃️

--- overlook_hotel/data/hotel_db.sql
+++ overlook_hotel/data/hotel_db.sql
@@ -46,6 +46,19 @@ CREATE TYPE public.role AS ENUM (
 ALTER TYPE public.role OWNER TO postgres;
 
 --
+-- Name: shift_name; Type: TYPE; Schema: public; Owner: postgres
+--
+
+CREATE TYPE public.shift_name AS ENUM (
+    'morning',
+    'afternoon',
+    'night'
+);
+
+
+ALTER TYPE public.shift_name OWNER TO postgres;
+
+--
 -- Name: shift_type; Type: TYPE; Schema: public; Owner: postgres
 --
 
@@ -327,7 +340,7 @@ ALTER SEQUENCE public.schedules_schedule_id_seq OWNED BY public.schedules.schedu
 
 CREATE TABLE public.shifts (
     shift_id integer NOT NULL,
-    employee_name character varying(100) NOT NULL,
+    name public.shift_name NOT NULL,
     start_time timestamp without time zone NOT NULL,
     end_time timestamp without time zone NOT NULL,
     type public.shift_type DEFAULT 'working'::public.shift_type NOT NULL,
@@ -336,6 +349,41 @@ CREATE TABLE public.shifts (
 
 
 ALTER TABLE public.shifts OWNER TO postgres;
+
+--
+-- Name: shifts_schedules; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.shifts_schedules (
+    id integer NOT NULL,
+    shift_id integer NOT NULL,
+    schedule_id integer NOT NULL
+);
+
+
+ALTER TABLE public.shifts_schedules OWNER TO postgres;
+
+--
+-- Name: shifts_schedules_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.shifts_schedules_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.shifts_schedules_id_seq OWNER TO postgres;
+
+--
+-- Name: shifts_schedules_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.shifts_schedules_id_seq OWNED BY public.shifts_schedules.id;
+
 
 --
 -- Name: shifts_shift_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
@@ -455,6 +503,13 @@ ALTER TABLE ONLY public.shifts ALTER COLUMN shift_id SET DEFAULT nextval('public
 
 
 --
+-- Name: shifts_schedules id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.shifts_schedules ALTER COLUMN id SET DEFAULT nextval('public.shifts_schedules_id_seq'::regclass);
+
+
+--
 -- Name: users user_id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -521,7 +576,18 @@ COPY public.schedules (schedule_id, user_id, shift_id, schedule_date) FROM stdin
 -- Data for Name: shifts; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.shifts (shift_id, employee_name, start_time, end_time, type) FROM stdin;
+COPY public.shifts (shift_id, name, start_time, end_time, type) FROM stdin;
+1	morning	2025-09-11 06:00:00	2025-09-11 14:00:00	working
+2	afternoon	2025-09-11 14:00:00	2025-09-11 22:00:00	working
+3	night	2025-09-11 22:00:00	2025-09-12 06:00:00	working
+\.
+
+
+--
+-- Data for Name: shifts_schedules; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.shifts_schedules (id, shift_id, schedule_id) FROM stdin;
 \.
 
 
@@ -583,10 +649,17 @@ SELECT pg_catalog.setval('public.schedules_schedule_id_seq', 1, false);
 
 
 --
+-- Name: shifts_schedules_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.shifts_schedules_id_seq', 1, false);
+
+
+--
 -- Name: shifts_shift_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.shifts_shift_id_seq', 1, false);
+SELECT pg_catalog.setval('public.shifts_shift_id_seq', 3, true);
 
 
 --
@@ -669,6 +742,14 @@ ALTER TABLE ONLY public.shifts
 
 
 --
+-- Name: shifts_schedules shifts_schedules_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.shifts_schedules
+    ADD CONSTRAINT shifts_schedules_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -725,6 +806,14 @@ ALTER TABLE ONLY public.reviews
 
 
 --
+-- Name: shifts_schedules fk_schedule; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.shifts_schedules
+    ADD CONSTRAINT fk_schedule FOREIGN KEY (schedule_id) REFERENCES public.schedules(schedule_id) ON DELETE CASCADE;
+
+
+--
 -- Name: schedules fk_schedule_shift; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -738,6 +827,14 @@ ALTER TABLE ONLY public.schedules
 
 ALTER TABLE ONLY public.schedules
     ADD CONSTRAINT fk_schedule_user FOREIGN KEY (user_id) REFERENCES public.users(user_id) ON DELETE CASCADE;
+
+
+--
+-- Name: shifts_schedules fk_shift; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.shifts_schedules
+    ADD CONSTRAINT fk_shift FOREIGN KEY (shift_id) REFERENCES public.shifts(shift_id) ON DELETE CASCADE;
 
 
 --
