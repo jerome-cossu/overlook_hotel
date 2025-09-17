d1b112591433fa27c470903a33feeb54d18b94bc
Emmanuelle Mellinand-Richier <mellinand.emmanuelle@gmail.com>
2025-09-17 11:28:38 +0200

    fix: booking repo done.

---
+++ overlook_hotel/src/main/java/com/example/overlook_hotel/controller/BookingController.java
@@ -0,0 +1,39 @@
+package com.example.overlook_hotel.controller;
+import com.example.overlook_hotel.model.Booking;
+import com.example.overlook_hotel.service.BookingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/bookings")
+@RequiredArgsConstructor
+public class BookingController {
+
+    private final BookingService bookingService;
+
+    @GetMapping
+    public List<Booking> getAllBookings() {
+        return bookingService.getAllBookings();
+    }
+
+    @GetMapping("/{id}")
+    public Booking getBookingById(@PathVariable Long id) {
+        return bookingService.getBookingById(id);
+    }
+
+    @PostMapping
+    public Booking createBooking(@RequestBody Booking booking) {
+        return bookingService.createBooking(booking);
+    }
+
+    @PutMapping("/{id}")
+    public Booking updateBooking(@PathVariable Long id, @RequestBody Booking booking) {
+        return bookingService.updateBooking(id, booking);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBooking(@PathVariable Long id) {
+        bookingService.deleteBooking(id);
+    }
+}
--- overlook_hotel/src/main/java/com/example/overlook_hotel/controller/UserController.java
+++ overlook_hotel/src/main/java/com/example/overlook_hotel/controller/UserController.java
@@ -9,7 +9,6 @@ import java.util.List;
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
-
     private final UserService userService;
 
     @GetMapping
